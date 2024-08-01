52cc5e657a767a9b23e4a9c3311f2980b230f1d0
RadostW <radost.waszkiewicz@gmail.com>
2024-08-01 17:13:21 +0200

    Tidy up comparisons folder

---
+++ literature_comparison/README.md
@@ -0,0 +1,8 @@
+Raw data for comparison was derived from the following publications
+
+Bibliography
+------------
+ - *Bubbles, Drops and Particles*; R. Clift, J. Grace, M. Weber (1978)
+ - *Electrochemical measurements of mass transfer between a sphere and liquid in motion at high Peclet numbers*; S. Kutateladze, V. Nakoryakov, M. Iskakov (1982)
+ - *Mass and heat transfer from fluid spheres at low Reynolds numbers*; Z. Feng, E. Michaelides (2000)
+ - *Heat transfer from spheres to flowing media*; H. Kramers (1946)
--- literature_comparison/comparison.py
+++ literature_comparison/comparison.py
@@ -2,73 +2,115 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-'''IMPORTING OUR DATA'''
-our_data = Path(__file__).parent / "data/our"
-our_data = np.loadtxt(our_data)
-#change effective surface into sherwood
-for n in range(len(our_data)):
-    our_data[n,1] = (our_data[n,0]*our_data[n,1])/(4*np.pi)
+parent_dir = Path(__file__).parent
 
-'''CLASSICAL CLIFT SOLUTION'''
-nclift = Path(__file__).parent / "data/clift.txt"
-nclift = np.loadtxt(nclift)
+numerical_path = parent_dir / "data/this_work.csv"
+numerical = np.loadtxt(numerical_path, delimiter=",", skiprows=1)
 
-def clif(pe):
-    return (1/2)*(1+(1+2*pe)**(1/3))
+def clift_approximation(pe):
+    return (1 / 2) * (1 + (1 + 2 * pe) ** (1 / 3))
 
-xargs = np.logspace(-1, 5, 300)
-aclift = clif(xargs)
+peclet_values = np.logspace(-1, 5, 300)
+analytic_clift = clift_approximation(peclet_values)
 
-'''EXPERIMENTAL DATA OF KUTA et.al., FENG et. al. and KRAMERS et.al.'''
-kuta = Path(__file__).parent / "data/kuta.txt"
-kuta = np.loadtxt(kuta)
+numerical_clift_path = parent_dir / "data/clift.csv"
+numerical_clift = np.loadtxt(numerical_clift_path, delimiter=",", skiprows=1)
 
-feng = Path(__file__).parent / "data/feng.txt"
-feng = np.loadtxt(feng)
+kutateladze_path = parent_dir / "data/kutateladze.csv"
+kutateladze = np.loadtxt(kutateladze_path, delimiter=",", skiprows=1)
 
-kramers = Path(__file__).parent / "data/kramers.txt"
-kramers = np.loadtxt(kramers)
+feng_path = parent_dir / "data/feng.csv"
+feng = np.loadtxt(feng_path, delimiter=",", skiprows=1)
 
-'''GIGAPLOT'''
+kramers_path = parent_dir / "data/kramers.csv"
+kramers = np.loadtxt(kramers_path, delimiter=",", skiprows=1)
+
+
+# Plot all data
 
 plt.figure(figsize=(8, 5))
-
-plt.rcParams.update({
-    "text.usetex": True,
-    "font.family": "Cambria"
-})
-
-
-# Plot Clift data
-plt.loglog(xargs, aclift, label='Clift approximation', color='gray', linestyle='--', linewidth=3, zorder = 1)
-plt.loglog(nclift[:,0], nclift[:,1], label='Clift solution', color='g', linestyle='--', linewidth=3, zorder = 1)
+plt.rcParams.update({"text.usetex": True, "font.family": "Cambria"})
 
 # Plot our data
-plt.loglog(our_data[:, 0], our_data[:, 1], label='Our solution', color='b', linewidth=2, zorder = 2)
+plt.loglog(
+    numerical[:, 0],
+    numerical[:, 1],
+    label="This work",
+    color="b",
+    linewidth=2,
+    zorder=2,
+)
+
+# Plot Clift data
+plt.loglog(
+    peclet_values,
+    analytic_clift,
+    label="Clift et al. (analytic)",
+    color="gray",
+    linestyle="--",
+    linewidth=3,
+    zorder=1,
+)
+plt.loglog(
+    numerical_clift[:, 0],
+    numerical_clift[:, 1],
+    label="Clift et al. (numerical)",
+    color="g",
+    linestyle="--",
+    linewidth=3,
+    zorder=1,
+)
 
 # Plot Kuta experimental data
-plt.scatter(kuta[:, 0], kuta[:, 1], label='Kuta et al.', color='r', marker='o', s=50, edgecolor='k', zorder = 3)
+plt.scatter(
+    kutateladze[:, 0],
+    kutateladze[:, 1],
+    label="Kutateladze et al.",
+    color="r",
+    marker="o",
+    s=50,
+    edgecolor="k",
+    zorder=3,
+)
 
 # Plot Feng experimental data
-plt.scatter(feng[:, 0], feng[:, 1], label='Feng et al.', color='orange', marker='^', s=50, edgecolor='k', zorder = 3)
+plt.scatter(
+    feng[:, 0],
+    feng[:, 1],
+    label="Feng et al.",
+    color="orange",
+    marker="^",
+    s=50,
+    edgecolor="k",
+    zorder=3,
+)
 
 # Plot Kramers experimental data
-plt.scatter(kramers[:, 0], kramers[:, 1], label='Kramers et al.', color='pink', marker='s', s=50, edgecolor='k', zorder = 3)
+plt.scatter(
+    kramers[:, 0],
+    kramers[:, 1],
+    label="Kramers et al.",
+    color="pink",
+    marker="s",
+    s=50,
+    edgecolor="k",
+    zorder=3,
+)
 
 # Logarithmic scale
-plt.xscale('log')
-plt.yscale('log')
-plt.xlim(0.5,10**5)
-plt.ylim(0.8,30)
+plt.xscale("log")
+plt.yscale("log")
+plt.xlim(0.5, 10**5)
+plt.ylim(0.8, 30)
 plt.xticks(fontsize=15)
 plt.yticks(fontsize=15)
 
 # Labels and Title
-plt.xlabel(r"Peclet Number $\left(Pe\right)$", fontsize=15)
-plt.ylabel(r'Sherwood Number $\left(Sh\right)$', fontsize=15)
+plt.xlabel(r"Peclet Number $\left(\mathrm{Pe}\right)$", fontsize=15)
+plt.ylabel(r"Sherwood Number $\left(\mathrm{Sh}\right)$", fontsize=15)
 
 # Legend
-plt.legend(fontsize=15)
+plt.legend(fontsize=15, frameon=False)
 
 
 # Show plot
