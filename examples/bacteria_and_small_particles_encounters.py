d50409cffbeee88ff865275f5cc10f3d42cabceb
RadostW <radost.waszkiewicz@gmail.com>
2024-12-27 19:09:45 +0100

    Change to display 4 species instead of 3

--- examples/bacteria_and_small_particles_encounters.py
+++ examples/bacteria_and_small_particles_encounters.py
@@ -75,36 +75,35 @@ def to_upper_band(r_bact):
     return np.max(to_max)
 
 
-r_bacteria_list = np.linspace(10 ** (-5), 5, 400)
-
+r_bacteria_list = np.linspace(10 ** (-5), 25, 1000)
 
 upper_band = [to_upper_band(10 ** (-6) * r_bacteria) for r_bacteria in r_bacteria_list]
 lower_band = [to_lower_band(10 ** (-6) * r_bacteria) for r_bacteria in r_bacteria_list]
 
 
-def make_length_scale(xargs, yargs, thickness, height, c, label, ax):
-    ax.plot(
+def make_length_scale(xargs, yargs, thickness, height, c, label, axes):
+    axes.plot(
         xargs,
         yargs,
         c=c,
         lw=thickness,
     )
-    ax.plot(
+    axes.plot(
         [xargs[0], xargs[0]],
         [yargs[0] - height, yargs[0] + height],
         c=c,
         lw=thickness,
     )
-    ax.plot(
+    axes.plot(
         [xargs[1], xargs[1]],
         [yargs[0] - height, yargs[0] + height],
         c=c,
         lw=thickness,
     )
 
-    axes[0].text(
+    axes.text(
         (xargs[0] + xargs[1]) / 2,
-        yargs[0] + 0.035,
+        yargs[0] + 0.1,
         label,
         c=c,
         va="top",
@@ -114,23 +113,19 @@ def make_length_scale(xargs, yargs, thickness, height, c, label, ax):
     return None
 
 
-width = 0.3
-space = 0.02
+images_aspect = 1
 
 image_path = parent_dir / "images" / "pelagibacter.jpg"
 pelagibacter = mpimg.imread(image_path)
 x_min, x_max, y_min_from_top = 100, 400, 100
-y_max_from_top = (
-    int(round((x_max - x_min) * ((1 / 3 - 2 * space) / width))) + y_min_from_top
-)
+y_max_from_top = int(round((x_max - x_min) * images_aspect) + y_min_from_top)
 pelagibacter = pelagibacter[y_min_from_top:y_max_from_top, x_min:x_max]
 
 image_path = parent_dir / "images" / "roseobacter.jpg"
 roseobacter = mpimg.imread(image_path)
 x_min, x_max, y_min_from_top = 0, 700, 100
 y_max_from_top = (
-    int(round((x_max - x_min) * ((1 / 3 - 2 * space) / width)) * (989 / 697))
-    + y_min_from_top
+    int(round((x_max - x_min) * images_aspect) + y_min_from_top) + y_min_from_top
 )
 roseobacter = roseobacter[y_min_from_top:y_max_from_top, x_min:x_max]
 
@@ -138,96 +133,94 @@ image_path = parent_dir / "images" / "synechococcus.png"
 synechococcus = mpimg.imread(image_path)
 x_min, x_max, y_min_from_top = 50, 450, 20
 y_max_from_top = (
-    int(round((x_max - x_min) * ((1 / 3 - 2 * space) / width))) + y_min_from_top
+    int(round((x_max - x_min) * images_aspect) + y_min_from_top) + y_min_from_top
 )
 synechococcus = synechococcus[y_min_from_top:y_max_from_top, x_min:x_max]
 
-fontsize = 15 * 1.3 * 11.5 / 16 * 27.5 / 33.5
+fontsize = 11.5
 marker_size = 80
 plt.rcParams.update({"text.usetex": True, "font.family": "Times", "savefig.dpi": 700})
 
-fig, axes = plt.subplots(
-    1,
+image_colum_width = 0.25  # multiples of plot width
+figure_aspect_ratio = 300 / 800
+
+fig = plt.figure(
+    constrained_layout=True,
+    figsize=(11.7, 11.7 * figure_aspect_ratio),
+)
+gs = fig.add_gridspec(
     2,
-    figsize=(19.5 * 0.6, 10.5 * 0.6),
-    sharey=True,
-    gridspec_kw={"width_ratios": [width, 1]},
+    3,
+    wspace=0.02,
+    hspace=0.02,
+    width_ratios=[image_colum_width, image_colum_width, 1],
 )
 
-axes[0].set_xlim(0, width)
-axes[0].set_ylim(0, 1)
+axes_nw = fig.add_subplot(gs[0, 0])
+axes_ne = fig.add_subplot(gs[0, 1])
+axes_sw = fig.add_subplot(gs[1, 0])
+axes_se = fig.add_subplot(gs[1, 1])
+axes_big = fig.add_subplot(gs[:, 2])
 
-axes[0].imshow(
-    synechococcus,
-    extent=(0, width, space, 1 / 3 - space),
-    origin="upper",
-)
-axes[0].imshow(
-    pelagibacter,
-    aspect="equal",
-    extent=(0, width, 1 / 3 + space, 2 / 3 - space),
-    origin="upper",
-)
-axes[0].imshow(
-    roseobacter,
-    extent=(0, width, 2 / 3 + space, 1 - space),
-    origin="upper",
-)
-axes[0].axis("off")
+for image_ax in [axes_nw, axes_ne, axes_sw, axes_se]:
+    image_ax.set_xlim(0, 1)
+    image_ax.set_ylim(0, 1)
+    image_ax.set_axis_off()
 
+for image_ax, image in zip(
+    [axes_nw, axes_ne, axes_sw, axes_se],
+    [roseobacter, pelagibacter, synechococcus, synechococcus],
+):
+    image_ax.imshow(image, aspect="equal", extent=(0, 1, 0, 1))
 
-dist = 0.01
-axes[0].text(
-    dist,
-    1 / 3 - space - dist,
-    r"\textit{Synechococcus}",
-    c="k",
-    va="top",
-    ha="left",
-    fontsize=fontsize,
-)
+for image_ax, text in zip(
+    [axes_nw, axes_ne, axes_sw, axes_se],
+    [
+        r"\textit{Roseobacter}",
+        r"\textit{Pelagibacter}",
+        r"\textit{Synechococcus}",
+        r"\textit{Synechococcus}",
+    ],
+):
+    image_ax.text(
+        0.05,
+        0.95,
+        text,
+        c="w",
+        va="top",
+        ha="left",
+        fontsize=fontsize,        
+        bbox=dict(boxstyle='square,pad=0.15',fc='0.2',ec='none'),
+    )
 
-axes[0].text(
-    dist,
-    2 / 3 - space - dist,
-    r"\textit{Pelagibacter}",
-    c="w",
-    va="top",
-    ha="left",
-    fontsize=fontsize,
-)
+for image_ax, text in zip(
+    [axes_nw, axes_ne, axes_sw, axes_se],
+    [
+        r"$0.6$ $\mu$m",
+        r"$2$ $\mu$m",
+        r"$7$ $\mu$m",
+        r"$7$ $\mu$m",
+    ],
+):
+    make_length_scale([0.1, 0.4], [0.1, 0.1], 0.5, 0.002, "w", text, image_ax)
 
-axes[0].text(
-    dist,
-    1 - space - dist,
-    r"\textit{Roseobacter}",
-    c="w",
-    va="top",
-    ha="left",
-    fontsize=fontsize,
-)
-make_length_scale(
-    [0.075, 0.18], [0.88, 0.88], 0.5, 0.002, "w", r"$0.6$ $\mu$m", axes[0]
-)
-make_length_scale(
-    [0.0173, 0.229 - 0.0573 - 0.04], [0.4, 0.4], 0.5, 0.002, "w", r"$2$ $\mu$m", axes[0]
-)
-make_length_scale([0.18, 0.248], [0.1, 0.1], 0.5, 0.002, "k", r"$5$ $\mu$m", axes[0])
+#
+# Complicated graph
+#
 
-
-axes[1].plot(
+axes_big.plot(
     r_bacteria_list,
     lower_band,
     c="k",
 )
 
-axes[1].plot(
+axes_big.plot(
     r_bacteria_list,
     upper_band,
     c="k",
 )
 
-axes[1].fill_between(
+axes_big.fill_between(
     r_bacteria_list,
     lower_band,
     np.zeros_like(r_bacteria_list),
@@ -235,7 +228,7 @@ axes[1].fill_between(
     color="#a0cbe8",
 )
 
-axes[1].fill_between(
+axes_big.fill_between(
     r_bacteria_list,
     upper_band,
     np.ones_like(r_bacteria_list),
@@ -244,26 +237,26 @@ axes[1].fill_between(
 )
 
 
-axes[1].text(
-    0.25,
-    0.1,
+axes_big.text(
+    0.15,
+    0.2,
     r"diffusion share",
     ha="center",
     va="top",
     fontsize=fontsize,
-    transform=axes[1].transAxes,
+    transform=axes_big.transAxes,
     color=tableau[0],
     weight="bold",
 )
 
-axes[1].text(
+axes_big.text(
     0.8,
     0.8,
     r"direct interceptions share",
     ha="center",
     va="top",
     fontsize=fontsize,
-    transform=axes[1].transAxes,
+    transform=axes_big.transAxes,
     color=tableau[1],
     weight="bold",
 )
@@ -274,18 +267,18 @@ curve = (np.array(upper_band) + np.array(lower_band)) * (1 / 2)
 # axes[1]plot(r_bacteria_list, y_values)
 
 added_text = CurvedText(
-    x=r_bacteria_list[100:],
-    y=curve[100:],
+    x=r_bacteria_list[25:] - 0.15,
+    y=curve[25:],
     text=r"changes with bigger particle $a$ and $U$",
     va="bottom",
-    axes=axes[1],
+    axes=axes_big,
     fontsize=fontsize,
 )
 
 for val in [0.3, 0.5, 2.8]:
-    axes[1].axvline(x=val, c="gray", ls="--")
+    axes_big.axvline(x=val, c="gray", ls="--")
 
-axes[1].text(
+axes_big.text(
     2.8,
     0.7,
     r"\textit{Synechococcus}",
@@ -295,7 +288,7 @@ axes[1].text(
     rotation=90,
 )
 
-axes[1].text(
+axes_big.text(
     0.5,
     0.8,
     r"\textit{Pelagibacter}",
@@ -305,7 +298,7 @@ axes[1].text(
     rotation=90,
 )
 
-axes[1].text(
+axes_big.text(
     0.3,
     0.9,
     r"\textit{Roseobacter}",
@@ -316,13 +309,15 @@ axes[1].text(
 )
 
 
-axes[1].set_xlim(0, r_bacteria_list[-1])
-axes[1].set_ylim(0, 1.001)
+axes_big.set_xlim(0.75 * 1e-1, r_bacteria_list[-1])
+axes_big.set_xscale("log")
 
-axes[1].set_xlabel(r"Size of small particles ($b$) [$\mu$m]", fontsize=fontsize)
-axes[1].set_ylabel(r"Partial contribution", fontsize=fontsize)
+axes_big.set_ylim(0, 1.001)
 
-axes[1].tick_params(which="both", labelsize=fontsize, left=True, labelleft=True)
+axes_big.set_xlabel(r"Size of small particles ($b$) [$\mu$m]", fontsize=fontsize)
+axes_big.set_ylabel(r"Partial contribution", fontsize=fontsize)
+
+axes_big.tick_params(which="both", labelsize=fontsize, left=True, labelleft=True)
 
 plt.subplots_adjust(wspace=0.08)
 tosave = parent_dir / "graphics/encounter_type_with_small_particles.pdf"
